60b80a02306958442b759e1459622b7f10d04334
ZXCroon <zxcroon@gmail.com>
2018-10-30 22:45:13 -0400

    const in defs.h: int -> unsigned int

--- utils/defs.h
+++ utils/defs.h
@@ -11,22 +11,22 @@ typedef unsigned int SlotNum;
 // Comparison Operators
 //
 typedef unsigned int CompOp;
-const int EQ_OP = 0;
-const int LT_OP = 1;
-const int GT_OP = 2;
-const int LE_OP = 3;
-const int GE_OP = 4;
-const int NE_OP = 5;
-const int NO_OP = 6;
+const unsigned int EQ_OP = 0;
+const unsigned int LT_OP = 1;
+const unsigned int GT_OP = 2;
+const unsigned int LE_OP = 3;
+const unsigned int GE_OP = 4;
+const unsigned int NE_OP = 5;
+const unsigned int NO_OP = 6;
 
 
 //
 // Attribute Types
 //
 typedef unsigned int AttrType;
-const int INT = 0;
-const int FLOAT = 1;
-const int STRING = 2;
+const unsigned int INT = 0;
+const unsigned int FLOAT = 1;
+const unsigned int STRING = 2;
 
 
 //
