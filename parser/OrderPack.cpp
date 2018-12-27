042d69476ddfffa5a3a610241a5df8cb1e9ed303
fsygd <fsygd1996@163.com>
2018-12-27 21:00:38 +0800

    implement basic operations ref: #8

--- parser/OrderPack.cpp
+++ parser/OrderPack.cpp
@@ -8,6 +8,10 @@ void OrderPack::process() {
         break;
     }
 
+    case EXIT: {
+        exit(0);
+    }
+
     case CREATE_DATABASE: {
         printf("DEBUG: CREATE %s\n", dbname);
         smm.createDb(dbname);
@@ -26,14 +30,52 @@ void OrderPack::process() {
     }
 
     case SHOW_TABLES: {
+        smm.showTables();
+        break;
+    }
+
+    case CREATE_TABLE: {
         // todo
         break;
     }
-    
-    case EXIT: {
-        //todo
-        printf("EXIT!\n");
-        exit(0);
+
+    case DROP_TABLE: {
+        smm.dropTable(tbname);
+        break;
     }
+
+    case DESC_TABLE: {
+        smm.showTable(tbname);
+        break;
+    }
+
+    case INSERT_VALUES: {
+        // todo
+        break;
+    }
+
+    case DELETE_VALUES: {
+        // todo
+        break;
+    }
+
+    case UPDATE_VALUES: {
+        // todo
+        break;
+    }
+
+    case SELECT_VALUES: {
+        // todo
+        break;
+    }
+
+    case CREATE_INDEX: {
+        smm.createIndex(tbname, colname);
+        break;
+    }
+
+    case DROP_INDEX: {
+        smm.dropIndex(tbname, colname);
+        break;
     }
 }
